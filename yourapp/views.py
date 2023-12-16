5c1e95a5848adc2c7f9d8704716825b8a8147642
xknows <alikng94@gmail.com>
2023-12-16 11:50:44 +0330

    complex query filtering ORM

--- yourapp/views.py
+++ yourapp/views.py
@@ -3,7 +3,13 @@ from store.models import Product
 
 
 def sayHi(request):
+    # simple
     # someThingForTest = Product.objects.filter(unitPrice__range=(0,10))
     # someThingForTest = Product.objects.filter(title__istartswith='Coffee')
-    someThingForTest = Product.objects.filter(lastUpdate__year=2021)
-    return render(request, 'hello.html',{'products':list(someThingForTest)})
+    # someThingForTest = Product.objects.filter(lastUpdate__year=2021)
+    # complex
+    # someThingForTest = Product.objects.filter(inventory__lt=10, unitPrice__gt=20)
+    # someThingForTest = Product.objects.filter(Q(inventory__lt=10) | Q(unitPrice__gt=20) )
+    someThingForTest = Product.objects.filter(
+        Q(inventory__lt=10) & ~Q(unitPrice__gt=20))
+    return render(request, 'hello.html', {'products': list(someThingForTest)})
