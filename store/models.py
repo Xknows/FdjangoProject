44e7240a3ae43f310edcc7fb728569aebbf53f42
xknows <alikng94@gmail.com>
2023-12-16 10:44:02 +0330

    Changes due to git error

--- store/models.py
+++ store/models.py
@@ -15,11 +15,12 @@ class Collection(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unitPrice = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     lastUpdate = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion)
+    slug = models.SlugField()
 
 
 class Customer(models.Model):
